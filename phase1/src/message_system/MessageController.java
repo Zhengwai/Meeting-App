7275b5b5a3f1dfc140eef09ccd6bf31f0c8e13ab
Sotaro Ogawa <sotaro.ogawa@mail.utoronto.ca>
2020-11-03 15:33:03 +0800

    messagecontroller stuf

--- phase1/src/message_system/MessageController.java
+++ phase1/src/message_system/MessageController.java
@@ -2,4 +2,15 @@ package message_system;
 
 public class MessageController {
     // Will handle user interaction
+
+    public MessageManager getMessages(String path) {
+        //TODO: Returns messagess in message manager requested by User
+        return null;
+    }
+
+    public void messageMessageManager(String msg, MessageManager msgMgr) {
+        //TODO: Take a String msg. Instantiate a new Message object and append the Message object to msgMgr.messages.
+    }
+
+
 }
