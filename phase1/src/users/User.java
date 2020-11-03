4313705db6f99a7b8b50f933056d02e802fa3b7b
Haoran Tian <ed.tian@mail.utoronto.ca>
2020-11-02 22:50:45 -0800

    Updated basic user class with additional attributes, methods.

--- phase1/src/users/User.java
+++ phase1/src/users/User.java
@@ -1,15 +1,52 @@
 package users;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class User {
-    private String firstName;
-    private String lastName;
-    private String email;
+    private UUID userID = UUID.randomUUID();
+    private String username;
     private String password;
+    private Map<Integer, Integer> chatList = new HashMap<Integer, Integer>();
+    private String email = "";
+    private String type = "user";
 
-    public User(String firstName, String lastName, String email, String password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
+    public User(String username, String password){
+        this.username = username;
         this.password = password;
     }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getChatHistoryID(int friendID) {
+        return chatList.get(friendID);
+    }
+
+    public ArrayList<Integer> getFriendsIDs(){
+        return new ArrayList<Integer>(chatList.keySet());
+    }
+
 }
