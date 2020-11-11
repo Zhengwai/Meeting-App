0dd3bf7789ae00eab50ebc6b2624fa85a5ef75f9
Sotaro Ogawa <sotaro.ogawa@mail.utoronto.ca>
2020-11-11 08:33:10 +0800

    messagecontroller stufs

--- phase1/src/message_system/MessageController.java
+++ phase1/src/message_system/MessageController.java
@@ -12,17 +12,27 @@ public class MessageController {
 
 
     // The Controller should no
-    public MessageManager getMessages(String path) {
-        //TODO: Returns messagess in message manager requested by User
-        return null;
+    public Message[] getMessages(UUID convoId, User sender) {
+        //TODO: Returns messagess in conversation requested by User
+        return sender.msgMgr.getMessages(convoId);
     }
 
 
-    public void messageMessageManager(String msg, MessageManager msgMgr) {
+    public void messageConversation(String msg, UUID convoid, User sender) {
         //TODO: Take a String msg. Instantiate a new Message object and append the Message object to msgMgr.messages.
+        //TODO: Add UUID senderID and MessageManager msgMgr to User class
+
+        UUID senderId = sender.senderID;
+        Message newMsg = Message(senderId, msg);
+        sender.msgMgr.sendMessage(convoid, newMsg);
+
     }
 
-    public void createMessageManager(String name, UUID id) {
+    public void createConversation(User receiver, User sender) {
+        //TODO: Add MessageManager msgMgr to User class
+
+        convoId = sender.msgMgr.newConversation();
+        receiver.msgMgr.addConversation(convoId);
 
     }
 
