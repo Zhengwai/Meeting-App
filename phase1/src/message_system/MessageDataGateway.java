0d143ed0e84259f37117791aeeb9a4e05098403a
Sotaro Ogawa <sotaro.ogawa@mail.utoronto.ca>
2020-11-15 02:37:20 +0800

    message gateway

--- phase1/src/message_system/MessageDataGateway.java
+++ phase1/src/message_system/MessageDataGateway.java
@@ -9,7 +9,7 @@ public class MessageDataGateway {
     public MessageManager getMessageManager(String path) {
         // TODO: Return the message manager requested by user from a MessageManager.ser file
         // Will need readFromFile method
-        return null;
+        return readFromFile(path);
     }
 
     // Not sure if we need this, could be useful if we wanted to implement showing the User all their unread messages
@@ -19,15 +19,28 @@ public class MessageDataGateway {
         return null;
     }
 
+    public void saveToFile(String path, MessageManager msgMgr) throws IOException {
+        OutputStream file = new FileOutputStream(path);
+        OutputStream buffer = new BufferedOutputStream(file);
+        ObjectOutput output = new ObjectOutputSream(buffer);
 
-    private void readFromFile(String path) throws ClassNotFoundException {
+        output.writeObject(msgMgr);
+        output.close();
+    }
+
+
+    private Messagemanager readFromFile(String path) throws ClassNotFoundException {
         try {
             InputStream file = new FileInputStream(path);
             InputStream buffer = new BufferedInputStream(file);
             ObjectInput input = new ObjectInputStream(buffer);
-            currentManager = (MessageManager) input.readObject();
+
+            MessageManager currentManager = (MessageManager) input.readObject();
             input.close();
+            return currentManager;
         } catch (IOException ex) {
+            logger.log(level.SEVER, "Cannot read from input file, returning a new MessageManager.", ex);
+            return new MessageManager();
             // Will be discussed in lecture tomorrow:
            // logger.log(Level.SEVERE, "Cannot read from input.", ex);
         }
