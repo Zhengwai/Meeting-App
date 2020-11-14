b710e1399c5933874e912cc879c36fd7bd5cd872
xiechess <chessy.xie@mail.utoronto.ca>
2020-11-13 21:14:28 -0500

    edited login presenter

--- phase1/tests/login_system/LoginPresenterTest.java
+++ phase1/tests/login_system/LoginPresenterTest.java
@@ -1,20 +1,10 @@
 package login_system;
 
-import gateway.Serialization;
 import users.*;
 
 import java.util.ArrayList;
 
 public class LoginPresenterTest {
-    LoginController l = new LoginController();
-    Serialization s = new Serialization();
-
-    private ArrayList<User> lst = s.deserializeToArrLstOfUser("/phase1/userManager.ser");
-
-    public void testLogin(){
-        l.login(l.promptEmail(),l.promptPassword(),lst);
-    }
-
 
 
 
