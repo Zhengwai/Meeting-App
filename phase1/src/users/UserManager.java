66005a51b20ca4f76a0b5555d46ba46035b68c2b
xiechess <chessy.xie@mail.utoronto.ca>
2020-11-13 20:10:49 -0500

    serialize allUsers in UserManager

--- phase1/src/users/UserManager.java
+++ phase1/src/users/UserManager.java
@@ -2,12 +2,16 @@ package users;
 
 import java.util.ArrayList;
 import java.util.UUID;
+import gateway.Serialization;
 
 public class UserManager {
     private ArrayList<User> allUsers;
+    Serialization s = new Serialization();
 
     public UserManager(User currentUser) {
         allUsers = new ArrayList<>();
+
+        s.serialize(allUsers);
     }
 
     public void addFriend(User user1, User user2){
