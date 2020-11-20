7277cddeeef94b88d616f78652888bb9e7a923e9
Haoran Tian <ed.tian@mail.utoronto.ca>
2020-11-20 14:41:04 -0800

    Added java doc for attendee, organizer and speaker.

--- phase1/src/users/Organizer.java
+++ phase1/src/users/Organizer.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-
+/**
+ * An attendee in the conference program.
+ */
 public class Organizer extends User implements Serializable {
     private UUID id = UUID.randomUUID();
     private String username;
@@ -12,12 +14,20 @@ public class Organizer extends User implements Serializable {
     private String password;
     private String type;
     private ArrayList<UUID> enrolledEvents = new ArrayList<UUID>();
-
+    /**
+     * Initializing an organizer.
+     * @param username username of this organizer.
+     * @param password password of this organizer.
+     */
     public Organizer(String username, String password) {
         super(username, password);
         type = "o";
     }
-
+    /**
+     * Returns the type of this user.
+     * The type is represented by <code>String</code>.
+     * @return Returns "o" representing this type of user as organizer.
+     */
     @Override
     public String getType(){
         return "o";
--- phase1/src/users/Speaker.java
+++ phase1/src/users/Speaker.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * A speaker in the conference program.
+ */
 public class Speaker extends User implements Serializable {
     private UUID id = UUID.randomUUID();
     private String username;
@@ -13,16 +16,33 @@ public class Speaker extends User implements Serializable {
     private String type;
     private ArrayList<UUID> eventIDs;
     private ArrayList<UUID> enrolledEvents = new ArrayList<UUID>();
+
+    /**
+     * Initializing a speaker.
+     * @param username username of this speaker
+     * @param password password of this password.
+     */
     public Speaker(String username, String password) {
         super(username, password);
         this.eventIDs = new ArrayList<UUID>();
         type = "s";
     }
+
+    /**
+     * Returns the type of this user.
+     * The type is represented by <code>String</code>.
+     * @return Returns "s" representing this type of user as speaker.
+     */
     @Override
     public String getType(){
         return "s";
     }
 
+    /**
+     * Returns list of ids of events that the speaker will hold.
+     * IDs of events are in form of <code>ArrayList<UUID></code>
+     * @return ArrayList of ids of the events.
+     */
     public ArrayList<UUID> getSpeakerEvents(){
         return this.eventIDs;
     }
