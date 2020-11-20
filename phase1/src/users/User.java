0bb8cd54075a9fce5e7cefae9f54b64ac40041a6
Haoran Tian <ed.tian@mail.utoronto.ca>
2020-11-20 14:26:33 -0800

    Added java doc for user.

--- phase1/src/users/User.java
+++ phase1/src/users/User.java
@@ -6,6 +6,11 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.UUID;
 
+/**
+ * A user in the conference program.
+ * The <code>User</code> type user only has basic functions and parameters. Most users would fall under one of its
+ * child class types: <code>Attendee</code>, <code>Organizer</code>, or <code>Speaker</code>
+ */
 public class User implements Serializable{
     private UUID id = UUID.randomUUID();
     private String username;
@@ -14,29 +19,71 @@ public class User implements Serializable{
     private ArrayList<UUID> conversations = new ArrayList<>();
     private ArrayList<UUID> friends = new ArrayList<>();
 
+    /**
+     * Initializes a user in the conference program.
+     * @param username the username of the user.
+     * @param password the password of the user.
+     */
     public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
+    /**
+     * Changes the password of this user.
+     * @param password the new password of this user.
+     */
     public void setPassword(String password){
         this.password = password;
     }
 
+    /**
+     * Returns the type of this user.
+     * The type is represented by a string. In <code>User</code> type of user, the type is defaulted as "User".
+     * @return the type of this user, "User".
+     */
     public String getType(){
         return "User";
     }
 
+    /**
+     * Returns the password of this user.
+     * The password is represented by <code>String</code>.
+     * @return the password of this user.
+     */
     public String getPassword(){return this.password;}
-
+    /**
+     * Returns the username of this user.
+     * The username is represented by <code>String</code>.
+     * @return the username of this user.
+     */
     public String getUsername(){return this.username;}
-
+    /**
+     * Returns the ID of this user.
+     * The ID is randomly generated upon initialization of the user and cannot be changed.
+     * The ID is unique.
+     * The ID is represented by <code>UUID</code>.
+     * @return the ID of this user.
+     */
     public UUID getID(){return this.id;}
-
+    /**
+     * Returns the IDs of this user's enrolled events.
+     * The IDs are randomly generated upon initialization of the events and cannot be changed.
+     * The IDs are unique.
+     * The IDs are represented by <code>ArrayList<UUID></code>.
+     * @return the IDs of this user's enrolled events.
+     */
     public ArrayList<UUID> getEnrolledEvents(){
         return this.enrolledEvents;
     }
-
+    /**
+     * Add the id of a conversation to this user's list of conversation IDs.
+     * The IDs are randomly generated upon initialization of the conversation and cannot be changed.
+     * The IDs are unique.
+     * The ID is represented by <code>UUID</code>.
+     * @param id the id of a conversation.
+     * @return true iff the conversation has successfully been added.
+     */
     public boolean addConversation(UUID id) {
         if (conversations.contains(id)){
             return false;
@@ -44,7 +91,14 @@ public class User implements Serializable{
         conversations.add(id);
         return true;
     }
-
+    /**
+     * Remove the id of a conversation from this user's list of conversation IDs.
+     * The IDs are randomly generated upon initialization of the conversation and cannot be changed.
+     * The IDs are unique.
+     * The ID is represented by <code>UUID</code>.
+     * @param id the id of a conversation.
+     * @return true iff the conversation has successfully been removed.
+     */
     public boolean removeConversation(UUID id){
         if (conversations.contains(id)){
             conversations.remove(id);
@@ -52,11 +106,24 @@ public class User implements Serializable{
         }
         return false;
     }
-
+    /**
+     * Check if this user's list of conversation IDs contains a certain ID.
+     * The IDs are randomly generated upon initialization of the conversation and cannot be changed.
+     * The IDs are unique.
+     * The ID is represented by <code>UUID</code>.
+     * @param id the id of a conversation.
+     * @return true iff the passed conversation ID exists in this user's list of conversation IDs.
+     */
     public boolean hasConversation(UUID id){
         return conversations.contains(id);
     }
-
+    /**
+     * Returns the IDs of this user's conversations.
+     * The IDs are randomly generated upon initialization of the events and cannot be changed.
+     * The IDs are unique.
+     * The IDs are represented by <code>ArrayList<UUID></code>.
+     * @return the IDs of this user's conversations.
+     */
     public UUID[] getConversations() {
         UUID[] output = new UUID[this.conversations.size()];
         for (int i = 0; i < this.conversations.size(); i++) {
@@ -65,10 +132,14 @@ public class User implements Serializable{
         return output;
     }
 
-    public boolean isInConversation(UUID id){
-        return conversations.contains(id);
-    }
-
+    /**
+     * Add the id of a conversation to this user's list of conversation IDs.
+     * The IDs are randomly generated upon initialization of the conversation and cannot be changed.
+     * The IDs are unique.
+     * The ID is represented by <code>UUID</code>.
+     * @param id the id of an event.
+     * @return true iff the conversation has successfully been added.
+     */
     public boolean addEvent(UUID id){
         if (enrolledEvents.contains(id)){
             return false;
@@ -76,7 +147,14 @@ public class User implements Serializable{
         enrolledEvents.add(id);
         return true;
     }
-
+    /**
+     * Remove the id of an event from this user's list of conversation IDs.
+     * The IDs are randomly generated upon initialization of the conversation and cannot be changed.
+     * The IDs are unique.
+     * The ID is represented by <code>UUID</code>.
+     * @param id the id of an event.
+     * @return true iff the event has successfully been removed.
+     */
     public boolean removeEvent(UUID id){
         if (enrolledEvents.contains(id)){
             enrolledEvents.remove(id);
@@ -85,12 +163,23 @@ public class User implements Serializable{
         return false;
 
     }
-
+    /**
+     * Check if this user's list of event IDs contains a certain ID.
+     * The IDs are randomly generated upon initialization of the conversation and cannot be changed.
+     * The IDs are unique.
+     * The ID is represented by <code>UUID</code>.
+     * @param id the id of an event.
+     * @return true iff the passed event ID exists in this user's list of conversation IDs.
+     */
     public boolean isInEvent(UUID id){
         return enrolledEvents.contains(id);
     }
 
-
+    /**
+     * Add another user's id to this user's list of friends' ids.
+     * @param id id of a user.
+     * @return true iff the friend has been successfully added.
+     */
     public boolean addFriend(UUID id) {
         if (friends.contains(id)){
             return false;
@@ -98,7 +187,11 @@ public class User implements Serializable{
         friends.add(id);
         return true;
     }
-
+    /**
+     * Remove another user's id to this user's list of friends' ids.
+     * @param id id of a user.
+     * @return true iff the friend has been successfully removed.
+     */
     public boolean deleteFriend(UUID id) {
         if (friends.contains(id)){
             friends.remove(id);
@@ -107,11 +200,25 @@ public class User implements Serializable{
         return false;
     }
 
+    /**
+     * Returns this users friends' ids.
+     * @return an array list of this user's friends' ids.
+     */
     public ArrayList<UUID> getFriends() {return this.friends;}
-
+    /**
+     * Checks if this user is a friend with another user.
+     * @param id the id of another user.
+     * @return true iff this user's list of friends contains id.
+     */
     public boolean isFriendWithID(UUID id){
         return friends.contains(id);
     }
+
+    /**
+     * Returns the string representation of this user.
+     * Shows this user's type and user name.
+     * @return the string representation of this user.
+     */
     @Override
     public String toString(){
         return getType() + ":" + " " + username;
