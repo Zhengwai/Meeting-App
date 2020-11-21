031d70672ebea956c47329efbb1e9c7add995024
Haoran Tian <ed.tian@mail.utoronto.ca>
2020-11-20 16:03:24 -0800

    Exception classes javadoc added.

--- phase1/src/ScheduleSystem/TimeConflictException.java
+++ phase1/src/ScheduleSystem/TimeConflictException.java
@@ -2,7 +2,13 @@ package ScheduleSystem;
 
 import java.sql.Time;
 
+/**
+ * An TimeConflictException exception class.
+ */
 public class TimeConflictException extends Exception{
+    /**
+     * Initiates the TimeConflictException.
+     */
     public TimeConflictException(){
         super("You have a time conflict with this event.");
     }
--- phase1/src/ScheduleSystem/UnableToCancelException.java
+++ phase1/src/ScheduleSystem/UnableToCancelException.java
@@ -1,6 +1,12 @@
 package ScheduleSystem;
 
+/**
+ * An UnableToCancelException exception class.
+ */
 public class UnableToCancelException extends Exception{
+    /**
+     * Initiates the UnableToCancelException.
+     */
     public UnableToCancelException(){
         super("Event was not signed up, cannot be cancelled.");
     }
