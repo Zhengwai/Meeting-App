82a9f3b2aefd29e010e98cdb6d68144ea8831392
Haoran Tian <ed.tian@mail.utoronto.ca>
2020-11-29 01:22:33 -0800

    Starting on UI

--- phase2/src/Main.java
+++
@@ -1,8 +0,0 @@
-import controllers.LoginController;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        LoginController lc = new LoginController();
-        lc.instantiatingMethod();
-    }
-}
---
+++ phase2/src/UI/LoginController.java
@@ -0,0 +1,17 @@
+package UI;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+
+public class LoginController {
+
+    @FXML
+    private Button loginButton;
+
+    public void loginButtonOnAction(ActionEvent event) {
+
+    }
+}
