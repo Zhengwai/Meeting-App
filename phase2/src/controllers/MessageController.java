f8f32ecd3d00c347f0e5270848c6a38647497872
Amiel <amielnurja@gmail.com>
2020-11-28 21:13:15 -0500

    Started MessageController refactor, attendee message actions done. Bugged assertion in ConversationManagerTest?

---
+++ phase2/src/controllers/MessageController.java
@@ -0,0 +1,65 @@
+package controllers;
+
+import controllers.actions.MessageAction;
+import controllers.actions.MessageActionClient;
+import use_cases.ConversationManager;
+import use_cases.UserManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.UUID;
+
+public class MessageController {
+    private ConversationManager cm;
+    private UserManager um;
+    private UUID userID;
+
+    public MessageController(UUID userID) {
+        //TODO: Need to fetch appropriate data from database (when it's ready of course).
+        this.cm = new ConversationManager();
+        this.um = new UserManager();
+        this.userID = userID;
+    }
+
+    public void run() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        MessageActionClient mac = new MessageActionClient(userID, um, cm); // mac wit da scope haha brrr
+        try {
+            String input;
+
+            do {
+                List<MessageAction> actions = mac.getUserActions();
+
+                //TODO: Handle in message presenter?
+                System.out.println("Please Enter Corresponding Choice: \n");
+                for (int i = 0; i < actions.size(); i++) {
+                    System.out.println((i + 1) + ". " + actions.get(i).getName());
+                }
+                System.out.println("'exit' to leave the messaging system.");
+                input = br.readLine();
+
+                if (input.matches("^[0-9]$")) {
+                    int idx = Integer.parseInt(input);
+
+                    //TODO: Finish adding remaining Message actions!
+                    // Action gets called here!
+                    if (0 < idx && idx < actions.size() + 1) {
+                        actions.get(idx - 1).run();
+                    }
+
+                } else {
+                    System.out.println("Invalid input!");
+                };
+
+            } while (!input.equals("exit"));
+
+        } catch (IOException e) {
+            System.out.println("Failed to read input.");
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- phase2/tests/entities/ConversationTest.java
+++ phase2/tests/entities/ConversationTest.java
@@ -1,4 +1,57 @@
+package entities;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
 import static org.junit.Assert.*;
+
 public class ConversationTest {
-  
+    @Test
+    public void testAddAndGetMessageIDs() {
+        Conversation c = new Conversation();
+        UUID dummyID1 = UUID.fromString("212acdde-31cb-11eb-adc1-0242ac120002");
+        UUID dummyID2 = UUID.fromString("212acdde-31cb-11eb-adc1-0242ac120003");
+        c.addMessageID(dummyID1);
+        c.addMessageID(dummyID2);
+
+        assert dummyID1 == c.getMessageIDs().get(0);
+        assert dummyID2 == c.getMessageIDs().get(1);
+    }
+
+    @Test
+    public void testAddAndGetMembers() {
+        Conversation c = new Conversation();
+        UUID dummyID1 = UUID.fromString("212acdde-31cb-11eb-adc1-0242ac120002");
+        UUID dummyID2 = UUID.fromString("212acdde-31cb-11eb-adc1-0242ac120003");
+        c.addMember(dummyID1);
+        c.addMember(dummyID2);
+
+        assert dummyID1 == c.getMembers().get(0);
+        assert dummyID2 == c.getMembers().get(1);
+    }
+
+
+    @Test
+    public void testRemoveMembers() {
+        Conversation c = new Conversation();
+        UUID dummyID1 = UUID.fromString("212acdde-31cb-11eb-adc1-0242ac120002");
+        UUID dummyID2 = UUID.fromString("212acdde-31cb-11eb-adc1-0242ac120003");
+        UUID dummyID3 = UUID.fromString("212acdde-31cb-11eb-adc1-0242ac120004");
+        c.addMember(dummyID1);
+        c.addMember(dummyID2);
+        c.removeMember(dummyID1);
+
+        assert c.getMembers().size() == 1;
+        assert c.getMembers().get(0) == dummyID2;
+
+        c.addMember(dummyID3);
+
+        assert  c.getMembers().get(1) == dummyID3;
+
+        c.removeMember(dummyID3);
+
+        assert c.getMembers().size() == 1;
+        assert c.getMembers().get(0) == dummyID2;
+    }
 }
