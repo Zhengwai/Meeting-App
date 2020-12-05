424421152e65d4e3caf0e53a2dc4ebce8dd2ada5
xiechess <chessy.xie@mail.utoronto.ca>
2020-12-05 18:24:19 -0500

    (last commit was only gui not functionality) Edited Organizer schedule menu gui

--- phase2/src/UI/OrganizerScheduleMenuController.java
+++ phase2/src/UI/OrganizerScheduleMenuController.java
@@ -3,16 +3,24 @@ package UI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
 
 public class OrganizerScheduleMenuController extends ScheduleMenuController{
 
     @FXML
     Button createEventButton;
+
+    @FXML
+    Button cancelEventButton;
     private String fxmlName = "OrganizerScheduleMenu.fxml";
 
     public OrganizerScheduleMenuController() throws ClassNotFoundException {
     }
 
     public void createEventButtonOnAction(ActionEvent event) {
+    }
+
+    public void cancelEventButtonOnAction(ActionEvent event) {
+        
     }
 }
