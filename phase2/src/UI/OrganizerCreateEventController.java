d8d8629f67cf74e219267e41a6bf15906a66ebff
xiechess <chessy.xie@mail.utoronto.ca>
2020-12-09 00:08:31 -0500

    organizer functionality update

---
+++ phase2/src/UI/OrganizerCreateEventController.java
@@ -0,0 +1,15 @@
+package UI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class OrganizerCreateEventController extends MenuController{
+    @FXML
+    Button createEventButton;
+    public OrganizerCreateEventController() throws ClassNotFoundException {
+    }
+
+    public void createEventButtonOnAction(ActionEvent actionEvent) {
+    }
+}
--- phase2/src/UI/OrganizerScheduleMenuController.java
+++ phase2/src/UI/OrganizerScheduleMenuController.java
@@ -2,8 +2,15 @@ package UI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class OrganizerScheduleMenuController extends ScheduleMenuController{
 
@@ -17,10 +24,19 @@ public class OrganizerScheduleMenuController extends ScheduleMenuController{
     public OrganizerScheduleMenuController() throws ClassNotFoundException {
     }
 
-    public void createEventButtonOnAction(ActionEvent event) {
+    public void createEventButtonOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("OrganizerCreateEvent.fxml"));
+        Parent parent = fxmlLoader.load();
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+
     }
 
     public void cancelEventButtonOnAction(ActionEvent event) {
         OrganizerCancelEventAlertBox.display();
     }
+
 }
