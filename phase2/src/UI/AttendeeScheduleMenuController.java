91815b1741685b4a9bd42e109810260477091f0f
xiechess <chessy.xie@mail.utoronto.ca>
2020-12-11 20:36:40 -0500

    Attendee cancel events graphical interface and implementation; some fxml edits

--- phase2/src/UI/AttendeeScheduleMenuController.java
+++ phase2/src/UI/AttendeeScheduleMenuController.java
@@ -1,15 +1,27 @@
 package UI;
 
+import entities.Event;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class AttendeeScheduleMenuController extends ScheduleMenuController{
     private String fxmlName = "AttendeeScheduleMenu.fxml";
 
     public AttendeeScheduleMenuController() throws ClassNotFoundException {
     }
-
+    //Iff confirmation == true, signup the user for selected event
     public void signUpButtonOnAction(ActionEvent event) {
+        Boolean confirmation = AttendeeSignUpEventAlertBox.display();
+        if (confirmation) {
+            String ename = eventTable.getSelectionModel().getSelectedItem().getName().toString();
 
+            Event e = mainModel.getEm().getEventByName(ename);
+
+            e.addAttendee(mainModel.getUserID());
+
+        }
     }
 }
