991f89633073951f17027f0afb77c04173b48082
xiechess <chessy.xie@mail.utoronto.ca>
2020-12-11 22:06:15 -0500

    small edits

--- phase2/src/UI/AttendeeSignedUpScheduleMenuController.java
+++ phase2/src/UI/AttendeeSignedUpScheduleMenuController.java
@@ -19,10 +19,13 @@ public class AttendeeSignedUpScheduleMenuController extends ScheduleMenuControll
     public void cancelButtonOnAction(ActionEvent event) {
         Boolean confirmation = AttendeeCancelEventAlertBox.display();
         if (confirmation){
-            String ename = eventTable.getSelectionModel().getSelectedItem().getName().toString();
+            String ename = eventTable.getSelectionModel().getSelectedItem().getName().getValue();
+
             Event e = mainModel.getEm().getEventByName(ename);
+
             e.removeAttendee(mainModel.getUserID());
             mainModel.getCurrentUser().removeEvent(e.getId());
+            cancelEvent.setText("Cancellation Successful");
         }
 
     }
